488d33031729c5b91ecbfb58b639679e0c0fde9f
dimuthx353 <dimuthx353@gmail.com>
2025-06-21 22:25:22 +0530

    Refactor product image source: update to use storage path for better asset management.

--- resources/views/productdetails.blade.php
+++ resources/views/productdetails.blade.php
@@ -27,7 +27,7 @@
                     </span>
                     @endif
                     <img
-                        src="{{ $product->image_url ?? 'https://images.unsplash.com/photo-1605000797499-95a51c5269ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80' }}"
+                        src="{{ asset('storage/')."/".$product->image ?? '' }}"
                         alt="{{ $product->name }}"
                         class="img-fluid rounded-3" />
                 </div>
